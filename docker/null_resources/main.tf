eded0f9742ecd293a008817117514ff84d902acc
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-18 02:41:09 +0530

    Work added with docker added

--- docker/null_resources/main.tf
+++ docker/null_resources/main.tf
@@ -18,7 +18,7 @@ resource "docker_container" "container_1" {
 resource "null_resource" "null_id" {
   provisioner "local-exec" {
     # command = "echo ${docker_container.container_1.name}:${docker_container.container_1.ip_address} >> docker_container.info"
-    command = "docker cp ./update-bash.sh ${docker_container.container_1.name}:/tmp/ && docker exec ${docker_container.container_1.name} bash -c 'ls /tmp/;chmod +x /tmp/update-bash.sh;/tmp/update-bash.sh'"
+    command = "docker cp ./bootstrap.sh ${docker_container.container_1.name}:/tmp/ && docker exec ${docker_container.container_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh'"
 
   }
 
@@ -27,8 +27,8 @@ resource "null_resource" "null_id" {
 
 # resource "null_resource" "file_provisioner_sample" {
 #   provisioner "file" {
-#     content     = "./update-bash.sh"
-#     destination = "/tmp/update-bash.sh"
+#     content     = "./bootstrap.sh"
+#     destination = "/tmp/bootstrap.sh"
 
 #   }
 
