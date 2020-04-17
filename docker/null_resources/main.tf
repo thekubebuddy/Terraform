a2ff69f86fbb9e7368f0565f3be8761901cc80e6
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-18 03:01:25 +0530

    Terrafor with docker added

--- docker/null_resources/main.tf
+++ docker/null_resources/main.tf
@@ -6,7 +6,7 @@ resource "docker_image" "container_image" {
 
 resource "docker_container" "container_1" {
   name  = var.container_name
-  image = docker_image.container_image.name
+  image = docker_image.container_image.latest
   ports {
 
     internal = var.internal_port
