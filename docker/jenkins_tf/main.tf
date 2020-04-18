99b973e7584933672a78c1074a68907d125431f7
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-18 19:39:51 +0530

    Terraform work with docker added

---
+++ docker/jenkins_tf/main.tf
@@ -0,0 +1,35 @@
+resource "docker_image" "jenkins_image" {
+  name = var.image_name
+}
+
+
+
+resource "docker_container" "jenkins_container" {
+  name  = var.container_name
+  image = docker_image.jenkins_image.latest
+
+  ports {
+    internal = "8080"
+    external = "8080"
+  }
+
+  volumes {
+    volume_name    = docker_image.jenkins_volume.name
+    container_path = "/var/jenkins_home"
+  }
+
+  # for connecting the underlying host docker socket 
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+}
+
+
+# It will create a volume in /var/lib/docker/volumes/
+resource "docker_volume" "jenkins_volume" {
+  name = "jenkins_data"
+}
+
+
+
---
+++ docker/jenkins_tf/var.tf
@@ -0,0 +1,8 @@
+variable "image_name" {
+  default = "jenkins:tfv1.0"
+}
+
+
+variable "container_name" {
+  default = "jenkins_container"
+}
