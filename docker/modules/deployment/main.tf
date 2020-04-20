5ae602251d891a51fe667bb7faa98dd1facd8816
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-21 00:58:13 +0530

    Added work relted to module

--- docker/modules/deployment/main.tf
+++ docker/modules/deployment/main.tf
@@ -1,7 +1,7 @@
 module "image_1" {
   source = "../image"
   # image_name = var.list_of_images[0]
-  image_name = [for image in list_of_images : image]
+  image_name = [for image in var.list_of_images : image]
 }
 
 module "image_2" {
@@ -12,7 +12,7 @@ module "image_2" {
 module "ghost_container" {
   source         = "../container"
   container_name = var.container_name
-  image          = module.image.image_name
+  image          = module.image_1.image_name
   int_port       = var.internal_port
   ext_port       = var.external_port
 
