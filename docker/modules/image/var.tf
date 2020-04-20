9e27c71020558128ff5a051f3116249a488a2869
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-21 00:54:06 +0530

    Added work relted to module

--- docker/modules/image/var.tf
+++ docker/modules/image/var.tf
@@ -1,10 +1,5 @@
-variable "list_of_images" {
+variable "image_name" {
   # default = "ubuntu@sha256:acad929ffeda349d0e8c311baf841cc5251d228db7fae4b3f43e54bddbb743de"
-  default = ["ghost:alpine", "ghost:latest"]
+  default = "ghost:alpine"
 
-}
-
-variable "my_count" {
-  description = "describe your variable"
-  default     = 2
 }
