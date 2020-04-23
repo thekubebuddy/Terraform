7817d661f48f97203da05599dcb5581e22f84649
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 18:33:46 +0530

    Added Terransible with docker

--- docker/terransible_with_docker/ubuntu/bootstrap.sh
+++ docker/terransible_with_docker/ubuntu/bootstrap.sh
@@ -8,7 +8,6 @@ apt install ansible -y
 apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -O https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip && unzip terraform_0.12.24_linux_amd64.zip -d /usr/local/bin/
 terraform -install-autocomplete
