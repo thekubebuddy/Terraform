ee23db0efd186da711ea3a59a2599dac785523e6
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 20:34:20 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/output.tf
+++ terransible_with_docker/module_1/output.tf
@@ -1,7 +1,7 @@
-output "container_name" {
-  value = docker_container.ubuntu_pod_1.name
-}
+# output "container_name" {
+#   value = docker_container.ubuntu_pod_1.name
+# }
 
-output "container_ip" {
-  value = docker_container.ubuntu_pod_1.ip_address
-}
+# output "container_ip" {
+#   value = docker_container.ubuntu_pod_1.ip_address
+# }
