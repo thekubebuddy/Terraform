0e9d36f6397a5602419f1748b5e5af3ff2e290c8
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 22:50:27 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/main.tf
+++ terransible_with_docker/module_1/main.tf
@@ -62,20 +62,21 @@ for container in ${join(" ", docker_container.ubuntu_pod.*.name)}
 do
 echo $container
 docker cp ./bootstrap.sh $container:/tmp/ 
+docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+rm ~/.ssh/known_hosts
 done
 EOD
   }
 }
-# docker cp ./bootstrap.sh ${docker_container.ubuntu_pod_1.name}:/tmp/ 
 
-# docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
-
-# rm ~/.ssh/known_hosts
-
+# for container in ${join(" ", docker_container.ubuntu_pod.*.ip_address)}
+# do
+# echo $container
 # sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod_1.ip_address} -o StrictHostKeyChecking=no
-
 # cat <<EOF>docker_hosts
 # [${docker_container.ubuntu_pod_1.name}]
 # ${docker_container.ubuntu_pod_1.ip_address}
 # EOF
 # ansible ${docker_container.ubuntu_pod_1.name} -i docker_hosts -m ping
+# docker cp ./bootstrap.sh $container:/tmp/ 
+# done
