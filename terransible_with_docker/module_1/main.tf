ad921d1448ffe956ecd15424f52419546e50a0eb
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 20:23:21 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/main.tf
+++ terransible_with_docker/module_1/main.tf
@@ -10,11 +10,17 @@ resource "docker_container" "ubuntu_pod_1" {
   name    = "ubuntu_1"
   image   = docker_image.image_id.latest
   command = ["/bin/sleep", "infinity"]
+
   provisioner "local-exec" {
     command = <<EOD
-docker cp ./bootstrap.sh ${docker_container.ubuntu_pod_1.name}:/tmp/ && docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+docker cp ./bootstrap.sh ${docker_container.ubuntu_pod_1.name}:/tmp/ 
+
+docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+
 rm ~/.ssh/known_hosts
+
 sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod_1.ip_address} -o StrictHostKeyChecking=no
+
 cat <<EOF>docker_hosts
 [${docker_container.ubuntu_pod_1.name}]
 ${docker_container.ubuntu_pod_1.ip_address}
@@ -22,5 +28,35 @@ EOF
 ansible ${docker_container.ubuntu_pod_1.name} -i docker_hosts -m ping
 EOD
   }
+
 }
 
+
+
+
+
+resource "docker_container" "ubuntu_pod" {
+  count   = 2
+  name    = "ubuntu_pod_${count}"
+  image   = docker_image.image_id.latest
+  command = ["/bin/sleep", "infinity"]
+
+  provisioner "local-exec" {
+    command = <<EOD
+docker cp ./bootstrap.sh ${docker_container.ubuntu_pod.name}:/tmp/ 
+
+docker exec ${docker_container.ubuntu_pod.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+
+rm ~/.ssh/known_hosts
+
+sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod.ip_address} -o StrictHostKeyChecking=no
+
+cat <<EOF>docker_hosts
+[${docker_container.ubuntu_pod.name}]
+${docker_container.ubuntu_pod.ip_address}
+EOF
+ansible ${docker_container.ubuntu_pod.name} -i docker_hosts -m ping
+EOD
+  }
+
+}
