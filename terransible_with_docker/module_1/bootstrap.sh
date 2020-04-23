b7f517c9f567e0d96d8fc20f0842d8e766921b3c
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 20:07:12 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/bootstrap.sh
+++ terransible_with_docker/module_1/bootstrap.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# installing the 
-apt-get update -y && apt install software-properties-common unzip git -y
-apt-add-repository ppa:ansible/ansible -y
-apt update
-apt install ansible -y
-# installing docker and terraform
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg > /tmp/dkey; apt-key add /tmp/dkey
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable"
-apt-get install -y docker-ce docker-ce-cli containerd.io
-curl -O https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip && unzip terraform_0.12.24_linux_amd64.zip -d /usr/local/bin/
-terraform -install-autocomplete
+# installing the open-ssh
+apt-get update -y && apt install software-properties-common unzip git vim openssh-server -y
+service ssh start
+sed -i 's|#   PasswordAuthentication yes|PasswordAuthentication yes |g' /etc/ssh/ssh_config
+sed -i 's|#   Port 22|Port 22|g' /etc/ssh/ssh_config
+sed -i 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|g' /etc/ssh/sshd_config
+echo  -e "root123\nroot123" | passwd root
+service ssh stop
+service ssh start
+
+# Configuring passwordless ssh witin container
+#ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa 2>/dev/null <<< y >/dev/null
+#cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
