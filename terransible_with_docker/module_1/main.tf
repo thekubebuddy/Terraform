44c975d72d72670a660c0335b78112a498c5cb00
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 22:47:34 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/main.tf
+++ terransible_with_docker/module_1/main.tf
@@ -58,10 +58,19 @@ resource "null_resource" "cluster" {
     # command = "bootstrap-cluster.sh ${join(" ", docker_container.ubuntu_pod.*.name)}"
     command = <<EOD
 echo ${join(" ", docker_container.ubuntu_pod.*.name)}
-for x in ${join(" ", docker_container.ubuntu_pod.*.name)}
+for container in ${join(" ", docker_container.ubuntu_pod.*.name)}
 do
-echo $x
+echo $container
+docker cp ./bootstrap.sh $container:/tmp/ 
 done
+# docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+# rm ~/.ssh/known_hosts
+# sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod_1.ip_address} -o StrictHostKeyChecking=no
+# cat <<EOF>docker_hosts
+# [${docker_container.ubuntu_pod_1.name}]
+# ${docker_container.ubuntu_pod_1.ip_address}
+# ansible ${docker_container.ubuntu_pod_1.name} -i docker_hosts -m ping
+
 EOD
   }
 }
