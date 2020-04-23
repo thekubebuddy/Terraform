c8028307c30279bbfeff081751ba4c3481ec9d62
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 20:32:49 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/main.tf
+++ terransible_with_docker/module_1/main.tf
@@ -6,30 +6,30 @@ resource "docker_image" "image_id" {
 
 
 # Start the Container
-resource "docker_container" "ubuntu_pod_1" {
-  name    = "ubuntu_1"
-  image   = docker_image.image_id.latest
-  command = ["/bin/sleep", "infinity"]
+# resource "docker_container" "ubuntu_pod_1" {
+#   name    = "ubuntu_1"
+#   image   = docker_image.image_id.latest
+#   command = ["/bin/sleep", "infinity"]
 
-  provisioner "local-exec" {
-    command = <<EOD
-docker cp ./bootstrap.sh ${docker_container.ubuntu_pod_1.name}:/tmp/ 
+#   provisioner "local-exec" {
+#     command = <<EOD
+# docker cp ./bootstrap.sh ${docker_container.ubuntu_pod_1.name}:/tmp/ 
 
-docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+# docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
 
-rm ~/.ssh/known_hosts
+# rm ~/.ssh/known_hosts
 
-sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod_1.ip_address} -o StrictHostKeyChecking=no
+# sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod_1.ip_address} -o StrictHostKeyChecking=no
 
-cat <<EOF>docker_hosts
-[${docker_container.ubuntu_pod_1.name}]
-${docker_container.ubuntu_pod_1.ip_address}
-EOF
-ansible ${docker_container.ubuntu_pod_1.name} -i docker_hosts -m ping
-EOD
-  }
+# cat <<EOF>docker_hosts
+# [${docker_container.ubuntu_pod_1.name}]
+# ${docker_container.ubuntu_pod_1.ip_address}
+# EOF
+# ansible ${docker_container.ubuntu_pod_1.name} -i docker_hosts -m ping
+# EOD
+#   }
 
-}
+# }
 
 
 
@@ -44,7 +44,14 @@ resource "docker_container" "ubuntu_pod" {
   provisioner "local-exec" {
     command = <<EOD
   docker cp ./bootstrap.sh ${docker_container.ubuntu_pod.*.name}:/tmp/ 
+  # docker exec ${docker_container.ubuntu_pod.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+  # rm ~/.ssh/known_hosts
+  # sshpass -p 'root123' ssh-copy-id root@${docker_container.ubuntu_pod.ip_address} -o StrictHostKeyChecking=no
+  # cat <<EOF>docker_hosts
+  # [${docker_container.ubuntu_pod.name}]
+  # ${docker_container.ubuntu_pod.ip_address}
   EOF
+  # ansible ${docker_container.ubuntu_pod.name} -i docker_hosts -m ping
   EOD
   }
 
