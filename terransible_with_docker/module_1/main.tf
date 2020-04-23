9da3c88a6ad7f96278d34f17e6e6bca2a3a6e558
thekubebuddy <shaikhishaq1996@gmail.com>
2020-04-23 20:11:22 +0530

    Added Terransible with docker

--- terransible_with_docker/module_1/main.tf
+++ terransible_with_docker/module_1/main.tf
@@ -13,6 +13,7 @@ resource "docker_container" "ubuntu_pod_1" {
   provisioner "local-exec" {
     command = <<EOD
 docker cp ./bootstrap.sh ${docker_container.ubuntu_pod_1.name}:/tmp/ && docker exec ${docker_container.ubuntu_pod_1.name} bash -c 'ls /tmp/;chmod +x /tmp/bootstrap.sh;/tmp/bootstrap.sh' | tee container.log
+rm ~/.ssh/known_hosts
 ssh-copy-id root@${docker_container.ubuntu_pod_1.ip_address} -o StrictHostKeyChecking=no
 cat <<EOF>>docker_hosts
 [${docker_container.ubuntu_pod_1.name}]
